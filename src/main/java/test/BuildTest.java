fe29073ab3dede29cf818144e7f43c0f3d438746
shubhammehta04 <shubham.mehta@sqelabs.com>
2023-02-16 10:38:16 +0530

    Uncommented

--- src/main/java/test/BuildTest.java
+++ src/main/java/test/BuildTest.java
@@ -18,42 +18,42 @@ public class BuildTest extends BaseTest {
 
 
 
-//	@Test(priority = 1, enabled = true, description = "verify Customer Tabs")
-//	public void verifyBuildVersions() {
-//	
-//		By copyright = By.xpath("/html/body/div[3]/p/span[1]");
-//		String[] arr = { "https://markettest.utilibill.com.au/marketbilling/",
-//           
-//				"https://utbr8.utilibill.com.au/billing/",          
-//				"https://nuruat.utilibill.com.au/nur/", 
-//				"https://utbr8uat.utilibill.com.au/billing",
-//				"https://test.utilitybilling.com/billing/",
-//				"https://go.utilitybilling.com/billing/",
-//				"https://test.utilitybilling.com/marketbilling/",
-//
-//		};
-//
-//		SoftAssert softAssert = new SoftAssert();
-//
-//		for (int i = 0; i < arr.length; i++) {
-//			String versiontext = "";
-//	
-//			try {
-//				getDriver().get(arr[i]);
-//				versiontext = getDriver().findElement(copyright).getText();
-//				extentTest.log(PASS, "==> Version Text " + versiontext);
-//
-//			} catch (Exception e) {
-//				extentTest.log(FAIL, "Exception getting Version Text " + versiontext);
-//
-//			}
-//			softAssert.assertEquals(versiontext, PropertiesUtil.getPropertyValue("test1", "buildversion.properties"));
-//
-//		}
-//
-//      softAssert.assertAll();
+	@Test(priority = 1, enabled = true, description = "verify Customer Tabs")
+	public void verifyBuildVersions() {
+	
+		By copyright = By.xpath("/html/body/div[3]/p/span[1]");
+		String[] arr = { "https://markettest.utilibill.com.au/marketbilling/",
+           
+				"https://utbr8.utilibill.com.au/billing/",          
+				"https://nuruat.utilibill.com.au/nur/", 
+				"https://utbr8uat.utilibill.com.au/billing",
+				"https://test.utilitybilling.com/billing/",
+				"https://go.utilitybilling.com/billing/",
+				"https://test.utilitybilling.com/marketbilling/",
 
-//	}
+		};
+
+		SoftAssert softAssert = new SoftAssert();
+
+		for (int i = 0; i < arr.length; i++) {
+			String versiontext = "";
+	
+			try {
+				getDriver().get(arr[i]);
+				versiontext = getDriver().findElement(copyright).getText();
+				extentTest.log(PASS, "==> Version Text " + versiontext);
+
+			} catch (Exception e) {
+				extentTest.log(FAIL, "Exception getting Version Text " + versiontext);
+
+			}
+			softAssert.assertEquals(versiontext, PropertiesUtil.getPropertyValue("test1", "buildversion.properties"));
+
+		}
+
+      softAssert.assertAll();
+
+	}
 
 	@Test(priority = 2, enabled = true, description = "verify Customer Tabs")
 	public void verifyBuild() {
