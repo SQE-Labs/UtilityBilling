1177120a2f092b824824cdeba0a80675847a8715
shubhammehta04 <shubham.mehta@sqelabs.com>
2023-03-13 18:16:43 +0530

    Added Payments and Allocation

--- src/main/java/test/BuildTest.java
+++ src/main/java/test/BuildTest.java
@@ -216,41 +216,41 @@ public class BuildTest extends BaseTest {
 //
 //	}
 
-	@Test(priority = 8, enabled = true, description = "verify Customer Tabs")
-	public void verifyBuild() {
-
-		By copyright = By.xpath("/html/body/div[3]/p/span[1]");
-		String[] arr = { "https://markettest.utilibill.com.au/marketbilling/",
-				"https://test.electricitybilling.com/billingtest/", "https://prod.electricitybilling.com/billing",
-				"https://test.waterworkslms.com/waterworks", "https://prod.waterworkslms.com/waterworks",
-				"https://testing.utilibill.com.au/billingtest", "https://tvs.utilibill.com.au/truevaluesolaruat",
-				"https://tvs.utilibill.com.au/truevaluesolar", "https://winenergy.utilibill.com.au/winenergyuat",
-				"https://winenergy.utilibill.com.au/winenergy", "https://edp.waterworkslms.com/edp",
-				"https://utbr8git.utilibill.com.au/billing/", "https://markettestv8.utilibill.com.au/marketbilling/",
-				"https://stanwelluat.utilibill.com.au/stanwell/", "https://stanwell5msuat.utilibill.com.au/stanwell/",
-				"https://powerclubuat.utilibill.com.au/powerclub/",
-				"https://futurexenergyuat.utilibill.com.au/futurexenergy", "https://utbrxuat.utilibill.com.au/utbrx/",
-				"https://go2.utilitybilling.com/billing/", "https://test2.utilitybilling.com/billing/",
-				"https://electricityinaboxuat.utilibill.com.au/electricityinabox/" };
-
-		SoftAssert softAssert = new SoftAssert();
-
-		for (int i = 0; i < arr.length; i++) {
-			String versiontext = "";
-			try {
-				getDriver().get(arr[i]);
-				versiontext = getDriver().findElement(copyright).getText();
-				extentTest.log(PASS, "==> Version Text " + versiontext);
-
-			} catch (Exception e) {
-				extentTest.log(FAIL, "Exception getting Version Text " + versiontext);
-
-			}
-			softAssert.assertNotNull(versiontext);
-
-		}
-		softAssert.assertAll();
-
-	}
+//	@Test(priority = 8, enabled = true, description = "verify Customer Tabs")
+//	public void verifyBuild() {
+//
+//		By copyright = By.xpath("/html/body/div[3]/p/span[1]");
+//		String[] arr = { "https://markettest.utilibill.com.au/marketbilling/",
+//				"https://test.electricitybilling.com/billingtest/", "https://prod.electricitybilling.com/billing",
+//				"https://test.waterworkslms.com/waterworks", "https://prod.waterworkslms.com/waterworks",
+//				"https://testing.utilibill.com.au/billingtest", "https://tvs.utilibill.com.au/truevaluesolaruat",
+//				"https://tvs.utilibill.com.au/truevaluesolar", "https://winenergy.utilibill.com.au/winenergyuat",
+//				"https://winenergy.utilibill.com.au/winenergy", "https://edp.waterworkslms.com/edp",
+//				"https://utbr8git.utilibill.com.au/billing/", "https://markettestv8.utilibill.com.au/marketbilling/",
+//				"https://stanwelluat.utilibill.com.au/stanwell/", "https://stanwell5msuat.utilibill.com.au/stanwell/",
+//				"https://powerclubuat.utilibill.com.au/powerclub/",
+//				"https://futurexenergyuat.utilibill.com.au/futurexenergy", "https://utbrxuat.utilibill.com.au/utbrx/",
+//				"https://go2.utilitybilling.com/billing/", "https://test2.utilitybilling.com/billing/",
+//				"https://electricityinaboxuat.utilibill.com.au/electricityinabox/" };
+//
+//		SoftAssert softAssert = new SoftAssert();
+//
+//		for (int i = 0; i < arr.length; i++) {
+//			String versiontext = "";
+//			try {
+//				getDriver().get(arr[i]);
+//				versiontext = getDriver().findElement(copyright).getText();
+//				extentTest.log(PASS, "==> Version Text " + versiontext);
+//
+//			} catch (Exception e) {
+//				extentTest.log(FAIL, "Exception getting Version Text " + versiontext);
+//
+//			}
+//			softAssert.assertNotNull(versiontext);
+//
+//		}
+//		softAssert.assertAll();
+//
+//	}
 
 }
